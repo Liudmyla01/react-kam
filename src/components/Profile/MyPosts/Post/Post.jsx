2d09377f8b6d5a4bfe5a87b7efb28ce89344f1e6
Людмила Билинец <lbilynets@MacBook-Air-Ludmila.local>
2021-10-10 20:02:12 +0300

    del 42

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = (props) => {
       <img src='https://lh3.googleusercontent.com/proxy/_0TWl_V7Z9Xm9mDF--IWy1uEUML0Q7Ffan7ZwpRiwiyn1NZufXd3DjElnOGgA7no4Z8MKv8CzFQsvbmcsaaruw6khNNh46OcXsix9ELmDhHoafospy7jXCAtKpT_PhnK2jR3Dxp1EPfBfGQzlcGfHRu92vXbk4qhL5XKEL0lyqqS7gLtMHZaOvOrhG2n2_Yp' />
         { props.message }
           <div>
-        <span>like</span> { props.likesCount }
+        <span>likes</span> { props.likesCount }
       </div>
     </div>
   )
