0cfff93555f1fbd387272ad82846e6a5f0d4ce17
Людмила Билинец <lbilynets@MacBook-Air-Ludmila.local>
2021-10-21 13:40:59 +0300

    les 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,34 +2,30 @@ import * as axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
 import ava from '../../assets/img/ava.jpeg'
-let Useres = (props) =>{
-    let getUsers = () => {
-        if (props.users.length ===0){
+
+class Users extends React.Component {
+    getUsers = () => {
+        if (this.props.users.length ===0){
             axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
+        }
     }
 
-    // props.setUsers([{id:1,photoSrc:'https://funpick.ru/wp-content/uploads/2018/02/Ava-pats-12.jpg',followed:false, fullName: 'Liudmyla', status: 'Frontend devel', location:{country: 'Ukraine', city:'Nikolaev'}},
-    // {id:2,photoSrc:'https://funpick.ru/wp-content/uploads/2018/02/Ava-pats-12.jpg',followed:true, fullName: 'Igor', status: 'machine master', location:{country: 'Ukraine', city:'Kiyev'}},
-    // {id:3,photoSrc:'https://funpick.ru/wp-content/uploads/2018/02/Ava-pats-12.jpg',followed:false, fullName: 'Tom', status: 'waiter', location:{country: 'Romania', city:'Bicaz'}},
-    // {id:4,photoSrc:'https://funpick.ru/wp-content/uploads/2018/02/Ava-pats-12.jpg',followed:true, fullName: 'Alex', status: 'Co-founder', location:{country: 'Ukraine', city:'Kiyev'}},
-    // {id:5,photoSrc:'https://funpick.ru/wp-content/uploads/2018/02/Ava-pats-12.jpg',followed:false, fullName: 'Dima', status: 'guru', location:{country: 'Belarus', city:'Minsk'}},
-    // ] )
-}
-    return <div>
+    render(){
+        return <div>
         <div>
-            <button onClick={getUsers}>getU</button>
+            <button onClick={this.getUsers}>getU</button>
         </div>
        {
-           props.users.map(u => {
+           this.props.users.map(u => {
              return <div  className={s.fontColor}>
                  <span>
                     <div><img src={u.photos.small != null ? u.photos.small : ava } alt="" className={s.uphoto} /></div>
                     <div> 
                         {u.followed 
-                        ? <button onClick={()=>{ props.unfollow(u.id)}}>Unfollow</button> 
-                        : <button onClick={()=>{ props.follow(u.id)}}>Follow</button>}
+                        ? <button onClick={()=>{ this.props.unfollow(u.id)}}>Unfollow</button> 
+                        : <button onClick={()=>{ this.props.follow(u.id)}}>Follow</button>}
                         
                     </div>
                  </span>
@@ -48,5 +44,8 @@ let Useres = (props) =>{
        }
     </div>
 }
+     
+}
 
-export default Useres;
+
+export default Users;
