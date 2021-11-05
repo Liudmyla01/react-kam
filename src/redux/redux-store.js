945f374aaded33b1243db5363a00a143754e93ba
Людмила Білинець <lbilynets@e2r7p2.unit.ua>
2021-11-05 14:14:06 +0200

    les 75

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,7 +3,8 @@ import dialogsReducer from './dialogs-reducer';
 import sideBarReducer from './sideBar-reducer';
 import usersReducer from './users-reducer';
 import authReducer from './auth-reducer';
-import  thunkMiddleware from 'redux-thunk'
+import  thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 const { createStore, combineReducers, applyMiddleware } = require("redux");
 
 
@@ -12,7 +13,8 @@ let redusers = combineReducers({
     dialogsPage: dialogsReducer,
     sideBar: sideBarReducer,
     userPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 //applyMiddleware(thunkMiddleware) - для внедрения thunk
